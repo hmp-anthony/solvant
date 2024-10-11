3315977e1434981c8461ad6a9d09684bfaa95688
hmp-anthony <anthonygoddard962@gmail.com>
2024-10-11 13:57:28 +0100

    updated cmake fetch to solve instruction error

--- cmake/FetchContent_Googlebenchmark.cmake
+++ cmake/FetchContent_Googlebenchmark.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     googlebenchmark
     GIT_REPOSITORY https://github.com/google/benchmark.git
-    GIT_TAG master
+    GIT_TAG "origin/main"
 )
 
 FetchContent_MakeAvailable(googlebenchmark)
